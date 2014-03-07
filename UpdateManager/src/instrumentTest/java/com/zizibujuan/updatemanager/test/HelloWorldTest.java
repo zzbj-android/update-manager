90d7d0903624da9d43223c568e8c2fd3cd22c99a
jzw <zhengwei.jin@gmail.com>
2014-03-07 22:36:29 +0800

    配置测试环境

---
+++ UpdateManager/src/instrumentTest/java/com/zizibujuan/updatemanager/test/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.zizibujuan.updatemanager.test;
+
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+/**
+ * Created by jzw on 14-3-7.
+ */
+public class HelloWorldTest extends AndroidTestCase {
+
+    @SmallTest
+    public void testA(){
+        assertTrue(true);
+    }
+}
